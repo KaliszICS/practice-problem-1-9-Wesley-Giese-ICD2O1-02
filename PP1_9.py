07f153cc4036289f337354204fa9ebd989afc225
Wesley-Giese-ICD2O1-02 <Wesley.giese@student.tdsb.on.ca>
2024-10-01 18:02:12 +0000

    m

--- PP1_9.py
+++ PP1_9.py
@@ -2,30 +2,32 @@
 
 def q1(): 
   #Write Assignment code here
-
-
+  print('"Hello World"')
 def q2(): 
   #Write Assignment code here
-
+  word3 = input("Input a word: ")
+  print(word3.lower())
+  print(word3.upper())
 
 def q3(): 
   #Write Assignment code here
-
-
+  word4 = input("Input a word that is at least 5 letters long: ")
+  print(word4[1:4])
 def q4(): 
   #Write Assignment code here
-
+  word5 = input("Input a word: ")
+  print(word5.index("o"))
 
 def q5(): 
   #Write Assignment code here
-
-
+  word6 = input("Input a word: ")
+  print(len(word6))
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+# q1()
+# q2()
+# q3()
+# q4()
+# q5()
